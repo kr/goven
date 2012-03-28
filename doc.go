5a0166ff2b05c09a9a4790e80377d23ba29c57b5
Keith Rarick <kr@xph.us>
2012-03-27 18:18:35 -0700

    more doc

--- doc.go
+++ doc.go
@@ -4,4 +4,11 @@
 //
 // Example usage:
 //   goven github.com/kr/pretty
+//
+// It takes the named package, clones its code into
+// a subdirectory, removes the VCS metadata, and
+// rewrites import paths in the current directory tree
+// to refer to the new location. For example, vendoring
+// github.com/kr/pretty into example.com/foo would
+// produce an import path of example.com/foo/github.com/kr/pretty.
 package main

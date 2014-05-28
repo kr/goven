kr/goven

commit fa2be858a13e77eb5eb2a3affb7ae07dedab3461

drop leading / from relative paths
goven would rewrite import paths in existing codebase of the form
    /_vendor/src/foo

which is wrong.

main.go
@@ -120,7 +120,11 @@ func lookupDir() (string, error) {
 	for _, top := range items {
 		top = filepath.Join(top, "src/")
 		if strings.HasPrefix(dot, top) {
-			return dot[len(top):], nil
+			cwd := dot[len(top):]
+			if cwd[0] == "/" {
+				cwd = cwd[1:]
+			}
+			return cwd, nil
 		}
 		if *debug {
 			log.Printf("cwd '%s' not found in GOPATH '%s'", dot, top)

commit bec215b102d0861eaad95d34df6638bc2b859f7e

string slices of length one are actually runes, not strings
This fixes a bug introduced in fa2be85.

main.go
@@ -121,7 +121,7 @@ func lookupDir() (string, error) {
 		top = filepath.Join(top, "src/")
 		if strings.HasPrefix(dot, top) {
 			cwd := dot[len(top):]
-			if cwd[0] == "/" {
+			if cwd[0] == '/' {
 				cwd = cwd[1:]
 			}
 			return cwd, nil